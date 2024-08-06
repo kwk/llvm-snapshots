f028807ea5b349b516ce2d411bef5ee0d317b334
Konrad Kleine <kkleine@redhat.com>
2024-08-06 22:36:15 +0200

    TestingFarmWatchResult: parse from json

--- snapshot_manager/snapshot_manager/testing_farm_util.py
+++ snapshot_manager/snapshot_manager/testing_farm_util.py
@@ -235,12 +235,12 @@ class TestingFarmRequest:
         self.adjust_env(self.chroot)
 
         request_id = sanitize_request_id(request_id=self.request_id)
-        cmd = f"testing-farm watch --no-wait --id {self.request_id}"
+        cmd = f"testing-farm watch --no-wait --format json --id {self.request_id}"
         # We ignore the exit code because in case of a test error, 1 is the exit code
         try:
             logging.info(f"Watching for testing-farm request: {cmd}")
             _, stdout, stderr = util.run_cmd(cmd=cmd, timeout_secs=40)
-            watch_result, artifacts_url = TestingFarmWatchResult.from_output(stdout)
+            watch_result, artifacts_url = TestingFarmWatchResult.from_json(stdout)
             if watch_result is None:
                 raise SystemError(
                     f"failed to watch 'testing-farm request': {cmd}\n\nstdout: {stdout}\n\nstderr: {stderr}"
@@ -480,50 +480,64 @@ class TestingFarmRequest:
         return urllib.parse.urlparse(url).hostname == "artifacts.osci.redhat.com"
 
 
-@enum.unique
-class TestingFarmWatchResult(enum.StrEnum):
-    """An enum to represent states from a testing-farm watch call
+@dataclasses.dataclass(kw_only=True, unsafe_hash=True)
+class TestingFarmWatchResult:
+    @enum.unique
+    class OverallState(enum.StrEnum):
+        ERROR = "error"
+        PASSED = "passed"
 
-    See https://gitlab.com/testing-farm/cli/-/blob/main/src/tft/cli/commands.py?ref_type=heads#L248-272
-    """
+        @classmethod
+        def all(cls) -> list:
+            return [s for s in TestingFarmWatchResult.OverallState]
 
-    REQUEST_WAITING_TO_BE_QUEUED = "request is waiting to be queued"  #  Package sources are being imported into Copr DistGit.
-    REQUEST_QUEUED = (
-        "request is queued"  # Build is waiting in queue for a backend worker.
-    )
-    REQUEST_RUNNING = (
-        "request is running"  # Backend worker is trying to acquire a builder machine.
-    )
-    TESTS_PASSED = "tests passed"  # Build in progress.
-    TESTS_FAILED = "tests failed"  # Successfully built.
-    TESTS_ERROR = "tests error"  # Build has been forked from another build.
-    TESTS_UNKNOWN = "tests unknown"  # This package was skipped, see the reason for each chroot separately.
-    PIPELINE_ERROR = "pipeline error"
+        @classmethod
+        def is_valid(cls, string: str) -> bool:
+            return string in cls.all()
+
+    @enum.unique
+    class State(enum.StrEnum):  #
+        COMPLETE = "complete"
+        RUNNING = "running"
+
+        @classmethod
+        def all(cls) -> list:
+            return [s for s in TestingFarmWatchResult.State]
+
+        @classmethod
+        def is_valid(cls, string: str) -> bool:
+            return string in cls.all()
+
+    overall: OverallState = None
+    """The overall state of the testing-farm request"""
+
+    state: State = None
+    """The state itself"""
+
+    artifacts: str = None
+    """The artifacts URL pointing to the testing-farm results"""
 
     def to_icon(self) -> str:
         """Get a github markdown icon for the given testing-farm watch result.
 
         See https://gist.github.com/rxaviers/7360908 for a list of possible icons."""
-        if self == self.REQUEST_WAITING_TO_BE_QUEUED:
-            return ":hourglass:"
-        if self == self.REQUEST_QUEUED:
-            return ":inbox_tray:"
-        if self == self.REQUEST_RUNNING:
+        if self.overall == self.OverallState.RUNNING:
             return ":running:"
-        if self == self.TESTS_PASSED:
-            return ":white_check_mark:"
-        if self == self.TESTS_FAILED:
-            return ":x:"
-        if self == self.TESTS_ERROR:
-            return ":x:"
-        if self == self.TESTS_UNKNOWN:
-            return ":grey_question:"
-        if self == self.PIPELINE_ERROR:
-            return ":warning:"
-
-    @classmethod
-    def all_watch_results(cls) -> list["TestingFarmWatchResult"]:
-        return [s for s in TestingFarmWatchResult]
+        return ":grey_question:"
+        # elif self.state == self.REQUEST_WAITING_TO_BE_QUEUED:
+        #     return ":hourglass:"
+        # elif self.state == self.REQUEST_QUEUED:
+        #     return ":inbox_tray:"
+        # elif self.state == self.TESTS_PASSED:
+        #     return ":white_check_mark:"
+        # elif self.state == self.TESTS_FAILED:
+        #     return ":x:"
+        # elif self.state == self.TESTS_ERROR:
+        #     return ":x:"
+        # elif self.state == self.TESTS_UNKNOWN:
+        #     return ":grey_question:"
+        # elif self.state == self.PIPELINE_ERROR:
+        #     return ":warning:"
 
     @property
     def is_complete(self) -> bool:
@@ -532,17 +546,13 @@ class TestingFarmWatchResult(enum.StrEnum):
 
         Examples:
 
-        >>> TestingFarmWatchResult("tests failed").is_complete
+        >>> TestingFarmWatchResult(overall="complete").is_complete
         True
 
-        >>> TestingFarmWatchResult("request is queued").is_complete
+        >>> TestingFarmWatchResult(overall="running").is_complete
         False
         """
-        return self.value in [
-            self.TESTS_PASSED,
-            self.TESTS_FAILED,
-            self.TESTS_ERROR,
-        ]
+        return self.overall in [self.OverallState.COMPLETE]
 
     @property
     def is_error(self) -> bool:
@@ -551,51 +561,26 @@ class TestingFarmWatchResult(enum.StrEnum):
 
         Examples:
 
-        >>> TestingFarmWatchResult("tests failed").is_complete
+        >>> TestingFarmWatchResult(overall="complete", state="error").is_complete
         True
 
-        >>> TestingFarmWatchResult("request is queued").is_complete
+        >>> TestingFarmWatchResult(overall="complete", state="passed").is_complete
+        True
+
+        >>> TestingFarmWatchResult(overall="running").is_complete
         False
         """
-        return self.value in [
-            self.TESTS_FAILED,
-            self.TESTS_ERROR,
+        return self.state in [
+            self.State.ERROR,
         ]
 
-    @property
-    def expect_artifacts_url(self) -> bool:
+    def expects_artifacts_url(self) -> bool:
         """Returns True if for the watch result we can expect and artifacts URL in the watch output."""
-        if self.value in [
-            self.REQUEST_WAITING_TO_BE_QUEUED,
-            self.REQUEST_QUEUED,
-            self.PIPELINE_ERROR,
-        ]:
-            return False
-        return True
+        return self.state == self.State.COMPLETE
 
     @classmethod
-    def is_watch_result(cls, string: str) -> bool:
-        """_summary_
-
-        Args:
-            string (str): _description_
-
-        Returns:
-            bool: _description_
-
-        Examples:
-
-        >>> TestingFarmWatchResult.is_watch_result('foo')
-        False
-
-        >>> TestingFarmWatchResult.is_watch_result('tests failed')
-        True
-        """
-        return string in cls.all_watch_results()
-
-    @classmethod
-    def from_output(cls, string: str) -> tuple["TestingFarmWatchResult", str]:
-        """Inspects the output of a testing-farm watch call and returns a tuple of result and artifacts url (if any).
+    def from_json(cls, string: str) -> tuple["TestingFarmWatchResult", str]:
+        """Inspects the JSON output of a testing-farm watch call and returns a tuple of result and artifacts url (if any).
 
         Args:
             string (str): The output of a testing-farm watch call.
@@ -604,51 +589,56 @@ class TestingFarmWatchResult(enum.StrEnum):
             tuple[str, TestingFarmWatchResult]: _description_
 
         Examples:
-        >>> s='''8J+UjiBhcGkgaHR0cHM6Ly9hcGkuZGV2LnRlc3RpbmctZmFybS5pby92MC4xL3JlcXVlc3RzLzI3
-        ... MWE3OWU4LWZjOWEtNGUxZC05NWZlLTU2N2NjOWQ2MmFkNArwn5qiIGFydGlmYWN0cyBodHRwOi8v
-        ... YXJ0aWZhY3RzLm9zY2kucmVkaGF0LmNvbS90ZXN0aW5nLWZhcm0vMjcxYTc5ZTgtZmM5YS00ZTFk
-        ... LTk1ZmUtNTY3Y2M5ZDYyYWQ0CuKdjCB0ZXN0cyBlcnJvcgpOb25lCg=='''
-        >>> import base64
-        >>> s = base64.b64decode(s).decode()
-        >>> TestingFarmWatchResult.from_output(s)
-        (<TestingFarmWatchResult.TESTS_ERROR: 'tests error'>, 'http://artifacts.osci.redhat.com/testing-farm/271a79e8-fc9a-4e1d-95fe-567cc9d62ad4')
-        >>> s='''8J+UjiBhcGkgaHR0cHM6Ly9hcGkuZGV2LnRlc3RpbmctZmFybS5pby92MC4xL3JlcXVlc3RzLzcy
-        ... ZWZiYWZjLTdkYjktNGUwNS04NTZjLTg3MzExNGE5MjQzNQrwn5ObIHBpcGVsaW5lIGVycm9yCkd1
-        ... ZXN0IGNvdWxkbid0IGJlIHByb3Zpc2lvbmVkOiBBcnRlbWlzIHJlc291cmNlIGVuZGVkIGluICdl
-        ... cnJvcicgc3RhdGUKCg=='''
-        >>> s = base64.b64decode(s).decode()
-        >>> TestingFarmWatchResult.from_output(s)
-        (<TestingFarmWatchResult.PIPELINE_ERROR: 'pipeline error'>, None)
-        >>> s='''8J+UjiBhcGkgaHR0cHM6Ly9hcGkuZGV2LnRlc3RpbmctZmFybS5pby92MC4xL3JlcXVlc3RzLzk3
-        ... YTdjYzI0LTY5MjYtNDA1OS04NGFjLWQwMDc4Mjk3YzMxOQrwn5qAIHJlcXVlc3QgaXMgcnVubmlu
-        ... Zwrwn5qiIGFydGlmYWN0cyBodHRwczovL2FydGlmYWN0cy5kZXYudGVzdGluZy1mYXJtLmlvLzk3
-        ... YTdjYzI0LTY5MjYtNDA1OS04NGFjLWQwMDc4Mjk3YzMxOQo='''
-        >>> s = base64.b64decode(s).decode()
-        >>> TestingFarmWatchResult.from_output(s)
-        (<TestingFarmWatchResult.REQUEST_RUNNING: 'request is running'>, 'https://artifacts.dev.testing-farm.io/97a7cc24-6926-4059-84ac-d0078297c319')
-        >>> s='''8J+UjiBhcGkgaHR0cHM6Ly9hcGkuZGV2LnRlc3RpbmctZmFybS5pby92MC4xL3JlcXVlc3RzLzg2
-        ... MGExZjdlLTA2NmMtNGU0Mi1iYWRkLThlNmRjYTkwYzE0Ygrwn5qiIGFydGlmYWN0cyBodHRwczov
-        ... L2FydGlmYWN0cy5vc2NpLnJlZGhhdC5jb20vdGVzdGluZy1mYXJtLzg2MGExZjdlLTA2NmMtNGU0
-        ... Mi1iYWRkLThlNmRjYTkwYzE0YgrinIUgdGVzdHMgcGFzc2VkCg=='''
-        >>> s = base64.b64decode(s).decode()
-        >>> TestingFarmWatchResult.from_output(s)
-        (<TestingFarmWatchResult.TESTS_PASSED: 'tests passed'>, 'https://artifacts.osci.redhat.com/testing-farm/860a1f7e-066c-4e42-badd-8e6dca90c14b')
+        >>> s='''
+        ... {
+        ...  "id": "271a79e8-fc9a-4e1d-95fe-567cc9d62ad4",
+        ...  "state": "complete",
+        ...  "artifacts": "http://artifacts.osci.redhat.com/testing-farm/271a79e8-fc9a-4e1d-95fe-567cc9d62ad4",
+        ...  "overall": "error",
+        ...  "arches_requested": [
+        ...    "x86_64"
+        ...  ],
+        ...  "errored_plans": {
+        ...    "N/A": [
+        ...      "/tests/snapshot-gating"
+        ...    ]
+        ...  },
+        ...  "failed_plans": {},
+        ...  "passed_plans": {}
+        ... }
+        ... '''
+        >>> TestingFarmWatchResult.from_json(s)
+        TestingFarmWatchResult(overall='error', state='complete', artifacts='http://artifacts.osci.redhat.com/testing-farm/271a79e8-fc9a-4e1d-95fe-567cc9d62ad4')
         """
-        string = clean_testing_farm_output(string)
-        for watch_result in TestingFarmWatchResult.all_watch_results():
-            if not re.search(pattern=str(watch_result), string=string):
-                continue
-            if not watch_result.expect_artifacts_url:
-                return (watch_result, None)
-            url_match = re.search(pattern=r"artifacts http[s]?://.*", string=string)
-            if not url_match:
-                raise ValueError(f"expected an artifacts URL but couldn't find one")
-            artifacts_url = str(
-                re.search(pattern=r"http[s]?://.*", string=url_match[0])[0]
-            ).strip()
-            return (watch_result, artifacts_url)
+        watch_result_json = json.loads(string)
 
-        return (None, None)
+        watch_result = TestingFarmWatchResult()
+
+        # Get overall field
+        if "overall" not in watch_result_json:
+            raise KeyError(f"failed to find 'overall' key in JSON: {string}")
+        overall_state = watch_result_json["overall"]
+        if not cls.OverallState.is_valid(overall_state):
+            raise ValueError(
+                f"'overall' value: '{overall_state}' is not part the overall states {cls.OverallState.all()}"
+            )
+        watch_result.overall = overall_state
+        # Get state
+        if "state" not in watch_result_json:
+            raise KeyError(f"failed to find 'state' key in JSON: {string}")
+        state = watch_result_json["state"]
+        if not cls.State.is_valid(state):
+            raise ValueError(
+                f"'state' value: '{state}' is not part the states {cls.State.all()}"
+            )
+        watch_result.state = state
+
+        if watch_result.expects_artifacts_url():
+            if "artifacts" not in watch_result_json:
+                raise KeyError(f"failed to find 'artifacts' key in JSON: {string}")
+            watch_result.artifacts = watch_result_json["artifacts"]
+
+        return watch_result
 
 
 def render_html(
